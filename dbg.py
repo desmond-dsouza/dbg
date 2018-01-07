bedfe0acc8bc6f2c058848ced6fa27ee04cc0b37
Desmond <des.1303.github@dsouzaville.com>
2018-01-06 23:35:10 -0600

    Works wtih python 2.7

--- dbg.py
+++ dbg.py
@@ -2,7 +2,7 @@
 # debugging(level): turn on or off all dbg stuff, or selectively enable
 # @TraceCalls() - decorator to trace entry & exit
 # debug(obj) - indented display of obj (returns obj, so can be used as "tap")
-# check(aLambda) - if aLambda does not evaluate to True, print error
+# check(aLambda_or_bool) - if aLambda does not evaluate to True, print error
 
 # Freely borrowed & adapted from others' code, sorry I did not keep track
 
@@ -11,25 +11,23 @@ import logging
 import sys
 from functools import wraps
 import pprint
-from enum import Enum
 
 
-class Dbg(Enum):
-    all = "All"
-    none = "Off"
-    enabled_only = "Enabled_Only"
+Dbg_all = "All"
+Dbg_none = "Off"
+Dbg_enabled_only = "Enabled_Only"
 
 
-DEBUGGING = Dbg.all
+DEBUGGING = Dbg_all
 
 
-def debugging(level: Dbg):
+def set_debugging(level):
     global DEBUGGING
     DEBUGGING = level
 
 
 def is_visible(enabled):
-    return DEBUGGING is Dbg.all or (DEBUGGING is Dbg.enabled_only and enabled)
+    return DEBUGGING is Dbg_all or (DEBUGGING is Dbg_enabled_only and enabled)
 
 
 class TraceCalls(object):
@@ -49,7 +47,7 @@ class TraceCalls(object):
             argstr = ', '.join(
                 [repr(a) for a in args] +
                 ["%s=%s" % (a, repr(b)) for a, b in kwargs.items()])
-            msg = f">> {fn.__name__}({argstr})"
+            msg = ">> %s(%s)" % (fn.__name__, argstr)
             _debug(msg, frame_index=2, traced=True, enabled=self.enabled)
 
             TraceCalls.cur_indent += self.indent_step
@@ -69,19 +67,19 @@ def _pp_indent(obj, indent=0, prefix=""):
 
 
 def debug(obj, label="", pretty=False, enabled=False):
-    """Display with indentation for debugging, returns obj. Optional label
-    and multi-line pretty-printing args, and enabled for selective debugging."""
+    """Display with indentation for debugging, returns obj.
+    Optional label and multi-line pretty-printing, and enabled for selective debugging."""
     return _debug(obj, label=label, pretty=pretty, enabled=enabled, frame_index=2)
 
 
 logging.basicConfig(format='%(message)s', level=0)
 
 
-def _debug(obj, label="", frame_index=1, traced=False, pretty=False, checked=None, enabled=False):
+def _debug(obj, label="", frame_index=1, traced=False, checked=None, pretty=False, enabled=False):
     """Internal use with additional args"""
     if not is_visible(enabled):
         return obj
-    YELLOW, GREEN, RED, RESET = "\u001b[33m", "\u001b[1;32m", "\u001b[1;31m", "\u001b[0m"
+    YELLOW, GREEN, RED, RESET = u"\u001b[33m", u"\u001b[1;32m", u"\u001b[1;31m", u"\u001b[0m"
     COLOR = YELLOW if checked is None else GREEN if checked is True else RED
     frame, filename, line_number, function_name, lines, index = inspect.getouterframes(
         inspect.currentframe())[frame_index]
@@ -91,8 +89,9 @@ def _debug(obj, label="", frame_index=1, traced=False, pretty=False, checked=Non
     call_padding = ' | ' * call_level
     tab_padding = '  ' * tab_level
     msg = _pp_indent(obj, indent=3, prefix=call_padding + tab_padding) if pretty else str(obj)
-    lbl = f"{label}: " if label else ""
-    logging.debug(f"{YELLOW}{line_number:<3d}{call_padding}{tab_padding} {COLOR}{lbl}{msg}{RESET}")
+    lbl = ("%s: " % label) if label else ""
+    logging.debug(u"%s%3d%s%s %s%s%s%s" % (YELLOW, line_number, call_padding, tab_padding, COLOR, lbl, msg, RESET))
+    # logging.debug(f"{YELLOW}{line_number:<3d}{call_padding}{tab_padding} {COLOR}{lbl}{msg}{RESET}")
     sys.stdout.flush()
     sys.stderr.flush()
     return obj
@@ -102,8 +101,8 @@ def check(msg, a_lambda, enabled=True):
     """If a_lambda() is False display FAILED message"""
     if not is_visible(enabled):
         return
-    if not a_lambda():
-        _debug(f"FAILED {msg} <<<<<<<<<<<<<<<<<<<<<<<<<<", frame_index=2, checked=False, enabled=enabled)
+    if (callable(a_lambda) and not a_lambda()) or (a_lambda is False):
+        _debug("FAILED %s <<<<<<<<<<<<<<<<<<<<<<<<<<" % msg, frame_index=2, checked=False, enabled=enabled)
     else:
-        _debug(f"PASSED {msg}", frame_index=2, checked=True, enabled=enabled)
+        _debug("PASSED %s : OK" % msg, frame_index=2, checked=True, enabled=enabled)
 
--- example_dbg.py
+++ example_dbg.py
@@ -1,19 +1,21 @@
 # An example of using dbg: recursive calls, trace, debug, check
 
-from dbg import debug, check, TraceCalls, debugging, Dbg
+from dbg import debug, check, TraceCalls
 
-debugging(Dbg.all)
+# set_debugging(...) to choose All, None, or Selective debugging
+from dbg import set_debugging, Dbg_all, Dbg_none, Dbg_enabled_only
+set_debugging(Dbg_all)
 
 
 @TraceCalls()
 def foo():
     debug('foo: Hi World', label="LABEL")
-    debug("foo: well")
+    debug([1, 2, 3])
     for i in range(3):
-        debug("foo: Now we're cookin")
-        check(f"invariant: {i} < 2", lambda: i < 2)
-        for i in range(debug(1, label="ONE")):
-            debug("foo: 2 tabs in", enabled=True)
+        debug("Now we're cookin")
+        check("invariant: %d < 2" % i, lambda: i < 2)
+        for j in range(debug(1, label="ONE")):
+            debug("nested loop", enabled=True)
     return "42"
 
 
@@ -24,7 +26,7 @@ pp_stuff.insert(0, pp_stuff[:])
 @TraceCalls(enabled=True)
 def bar(i, j=None):
     debug("bar " + str(i))
-    debug(pp_stuff, pretty=False)
+    debug(pp_stuff)
     if i > 1:
         bar(i - 1)
         return "99", i
